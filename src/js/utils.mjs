b38d057ba8a7095868eaf1ff8c3000c922f18eae
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-07-24 18:21:26 -0500

    fix erros wiht the links

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,11 +1,11 @@
 export function loadHeaderFooter() {
-  fetch('../partials/header.html')
+  fetch('/partials/header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
     });
 
-  fetch('../partials/footer.html')
+  fetch('/partials/footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
